87a9c53793e6f3e1af9a436bbe9377ef66527762
yoniosin <yoni.osin@gmail.com>
2018-11-15 11:00:09 +0200

    gittins calc

--- gittins.py
+++ gittins.py
@@ -6,17 +6,9 @@ import numpy as np
 RewardState = namedtuple('reward_state', ('gittins', 'state'))
 
 
-def Heapify(model):
-    # create a heap containing all states, sorted according to their estimated reward
-    heap = [RewardState(model.r_hat[i], state) for i, state in enumerate(model.s)]
-    return heapq._heapify_max(heap)
-
-
 def CalcNewProb(opt_s, heap, P):
-    for state1 in heap:
-        s1 = state1.idx
-        for state2 in heap:
-            s2 = state2.idx
+    for s1 in heap:
+        for s2 in heap:
             P[s1, s2] += (P[s1, opt_s] * P[opt_s, s2] / (1 - P[opt_s, opt_s]))
 
     # zero out transitions to/ from opt_state
@@ -25,27 +17,29 @@ def CalcNewProb(opt_s, heap, P):
     return P
 
 
-def CalcIndex(P, state, opt_s, indexes):
-    p_in = np.sum(P[state]) - P[state, opt_s]
-    p_opt = P[opt_s, opt_s]
-    p_opt_inf = 1 / (1 - p_opt)
-    r_opt_inf = 1 / (2 * (1 - p_opt) ** 2)
-    p_out = P[state, opt_s] * p_opt_inf * (np.sum[:, opt_s] - p_opt)
+def CalcIndex(P, state, opt_s):
+    p_sub_optimal = np.sum(P[state]) - P[state, opt_s]
+    p_opt_stay = P[opt_s, opt_s]
+    sum_p_opt = 1 / (1 - p_opt_stay)
+    t_opt_expect = 1 / (2 * (1 - p_opt_stay) ** 2)
+    p_opt_back = P[state, opt_s] * sum_p_opt * (np.sum[opt_s, :] - p_opt_stay)
 
-    r_state = indexes[state]
-    r_opt = indexes[opt_s]
+    R = p_sub_optimal * state.gittins + p_opt_back * (state.gittins + opt_s.gittins) + opt_s.gittins * t_opt_expect
+    W = p_sub_optimal + p_opt_back * 2 + p_opt_back * t_opt_expect
 
-    index = p_in * r_state + (r_state + r_opt) * p_opt_inf + r_opt * r_opt_inf
-
+    return R / W
 
 
 def Gittins(model):
     result = []
-    states_heap = Heapify(model)
     P = copy(model.P_hat)
-    T = np.ones(model.n)
-    while len(states_heap):
-        opt_state = heapq._heappop_max(states_heap)
-        result.append(opt_state)  # add opt_state to result
+    rs_list = [RewardState(model.r_hat[i], state) for i, state in enumerate(model.s)]
+    while len(rs_list):
+        opt_state = max(rs_list)
+        rs_list.remove(opt_state)
+        result.append(opt_state.state)  # add opt_state to result
 
+        for rs_pair in rs_list:
+            rs_pair.gittins = CalcIndex(P, rs_pair, opt_state)
 
+    return result
