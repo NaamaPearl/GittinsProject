3b851f4b388429d1de764ecc14bd4e57c3518591
yoniosin <yoni.osin@gmail.com>
2019-06-01 15:18:47 +0300

    added plot flag

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     ''''Parse user's arguments'''
     parser = argparse.ArgumentParser()
     parser.add_argument('--load_mdps', '-l', action='store_true', help='load existing MDP')
+    parser.add_argument('--plot', '-p', action='store_true', help='plot results')
     args = parser.parse_args()
 
     '''define general simulation params. Add arguments to constructor, or use default values'''
@@ -14,4 +15,6 @@ if __name__ == '__main__':
 
     runner = Runner(general_sim_params, args.load_mdps)
     runner.run()
-    runner.plot(['tree'])
+
+    if args.plot:
+        runner.plot(['tree'])
